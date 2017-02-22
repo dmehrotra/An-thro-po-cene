1969cfe8f95cd57c23396da4a02d1d69d4836cc4
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2017-02-22 11:53:11 -0500

    css

---
+++ sixth-extinction/extension/future_perfect/app.js
@@ -0,0 +1,9 @@
+
+window.setTimeout(function(){
+$('#main_2_splitleftarea_scexp1_0_imgScImageNoIntro').attr('src',"http://placekitten.com/g/300/300")	
+$('#main_3_1columnarea_scexp1_1_F03FeaturedItem_ImageFeatureItemID').attr('src',"http://placekitten.com/g/300/300")
+}, 500);
+ 
+
+
+
